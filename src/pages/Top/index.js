a178b9bd853d80c9191f4da23c21021fd33f5c37
João Horta <jpamarohorta@gmail.com>
2020-04-10 01:39:05 +0100

    feat: Top Chef page redesign

--- src/pages/Top/index.js
+++ src/pages/Top/index.js
@@ -3,12 +3,16 @@ import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from 'react-redux'
 import Spinner from 'react-spinkit'
 import { orderBy } from 'lodash'
+import Carousel from '@brainhubeu/react-carousel'
 
 import { actions as recipesActions, selectors as recipesSelectors } from 'store/reducers/recipes'
 
+import RecipeCard from 'components/RecipeCard'
 import ChefCard from 'components/ChefCard'
 
-import { logChefClicked } from 'utils/analytics'
+import { logChefClicked, logRecipeClicked } from 'utils/analytics'
+
+import '@brainhubeu/react-carousel/lib/style.css';
 
 const sortRecipesByChef = (recipes) => {
   let chefsObject = {}
@@ -32,13 +36,18 @@ const sortRecipesByChef = (recipes) => {
 
 const Top = () => {
   const [chefs, setChefs] = useState(null)
+  const [topChefRecipes, setTopChefRecipes] = useState(null)
 
   const dispatch = useDispatch()
 
   const recipes = useSelector(recipesSelectors.getRecipes)
 
-  const handleRecipeClicked = useCallback((slug) => {
+  const handleChefClicked = useCallback((slug) => {
     logChefClicked({ slug })
+  }, [])
+
+  const handleRecipeClicked = useCallback((slug) => {
+    logRecipeClicked(slug)
   }, [])
 
   useEffect(() => {
@@ -49,7 +58,9 @@ const Top = () => {
 
   useEffect(() => {
     if (recipes) {
-      setChefs(sortRecipesByChef(recipes))
+      const orderedChefs = sortRecipesByChef(recipes)
+      setChefs(orderedChefs)
+      setTopChefRecipes(recipes.filter((r) => r.chef === orderedChefs[0].name))
     }
   }, [recipes])
 
@@ -59,27 +70,55 @@ const Top = () => {
         <>
           <div className="top-chef">
             <div className="top-chef-info uk-container">
-              <h4>O Top Chef atual é</h4>
+              <p>O nosso top chef é</p>
               <Link
                 to={`chefs/${chefs[0].slug}`}
-                onClick={() => handleRecipeClicked(chefs[0].slug)}>
+                onClick={() => handleRecipeClicked(chefs[0].slug)}
+                className="top-chef-name">
                 <h1>{chefs[0].name}</h1>
               </Link>
-              <h4>{`${chefs[0].recipes.length} ${chefs[0].recipes.length === 1 ? 'receita' : 'receitas'}`}</h4>
+              <p>{`${chefs[0].recipes.length} ${chefs[0].recipes.length === 1 ? 'receita' : 'receitas'}`}</p>
             </div>
           </div>
 
           <div className="content-container">
             <div className="content uk-container">
-              <div className="chefs-list">
-                { chefs.slice(1).map((chef, index) => (
-                  <Link
-                    to={`chefs/${chef.slug}`}
-                    key={index}
-                    onClick={() => handleRecipeClicked(chef.slug)}>
-                    <ChefCard chef={chef} />
-                  </Link>
-                ))}
+              <div className="discover-section">
+                <div className="discover-section-header">
+                  <h4>A descobrir</h4>
+                  <p>Descubra as especialidades do nosso top chef</p>
+                </div>
+
+                <div className="discover-recipe-list">
+                  <Carousel slidesPerPage={4} offset={32} animationSpeed={250}>
+                    { topChefRecipes?.map((recipe, index) => (
+                      <Link
+                        to={`/recipes/${recipe.slug}`}
+                        key={index}
+                        className="recipe"
+                        onClick={() => handleChefClicked(recipe.slug)}>
+                        <RecipeCard recipe={recipe} key={index} />
+                      </Link>
+                    ))}
+                  </Carousel>
+                </div>
+              </div>
+
+              <div className="chefs-section">
+                <div className="chefs-section-header">
+                  <h4>Todos os chefs</h4>
+                  <p>Conheça todos os chefs têm contribuído para este projecto</p>
+                </div>
+                <div className="chefs-list">
+                  { chefs.slice(1).map((chef, index) => (
+                    <Link
+                      to={`chefs/${chef.slug}`}
+                      key={index}
+                      onClick={() => handleRecipeClicked(chef.slug)}>
+                      <ChefCard chef={chef} />
+                    </Link>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
